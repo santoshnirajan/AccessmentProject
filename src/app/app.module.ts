144073ae8ca3adfc8fbfd32e2fc1edef5f3e2f38
santoshnirajan <santosh.shahi475@gmail.com>
2024-05-28 11:46:24 +0545

    Components added to app Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductSearchComponent } from './product-search/product-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductEditComponent,
+    ProductSearchComponent
   ],
   imports: [
     BrowserModule,
