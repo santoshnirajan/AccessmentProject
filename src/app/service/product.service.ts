4e6084a167f9366a0bf14c07e6680e09da781f8b
santoshnirajan <santosh.shahi475@gmail.com>
2024-05-28 19:19:16 +0545

    Product Service Created

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Environment } from '../environment/environment.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductDto } from '../model/product.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private _environment: Environment) { }
+  constructor(
+    private _environment: Environment,
+    private _httpService: HttpClient) { }
 
   addProduct() {
 
   }
 
-  getProduct() {
-
+  getProducts(): Observable<any> {
+    return this._httpService.get(this._environment.baserUrl);
+  }
+  getProductById(productId: number): Observable<any> {
+    return this._httpService.get(`${this._environment.baserUrl}/` + productId);
   }
 
-  editProduct() {
-
+  editProduct(productId: number, productDetail: ProductDto): Observable<any> {
+    return this._httpService.put(`${this._environment.baserUrl}/` + productId, productDetail);
   }
   searchProduct() {
 
