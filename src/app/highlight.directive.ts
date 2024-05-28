b4119e7ac92e8dc9f2f972bf884a8a4b159e7517
santoshnirajan <santosh.shahi475@gmail.com>
2024-05-28 19:22:51 +0545

    Custom Directive for highlight created

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+  @Input() defaultColor: string = 'yellow';
+  @Input('appHighlight') highlightColor: string = '';
+
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || this.defaultColor);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', color);
+  }
+}
