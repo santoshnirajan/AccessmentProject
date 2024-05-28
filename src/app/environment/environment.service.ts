2141c16b723a9663d02fa738e64aa88dd9413fe1
santoshnirajan <santosh.shahi475@gmail.com>
2024-05-28 11:47:38 +0545

    Product service created for api and environment service created for application environment

---
+++ src/app/environment/environment.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Environment {
+  type: string = "Development";
+
+  baserUrl: string = "https://66556dc93c1d3b60293971ec.mockapi.io/api/";
+}
---
+++ src/app/service/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Environment } from '../environment/environment.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private _environment: Environment) { }
+
+  addProduct() {
+
+  }
+
+  getProduct() {
+
+  }
+
+  editProduct() {
+
+  }
+  searchProduct() {
+
+  }
+}
