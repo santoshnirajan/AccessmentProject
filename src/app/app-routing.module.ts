a480a552e61ec51bf1a1e076c5c72a5277d318bf
santoshnirajan <santosh.shahi475@gmail.com>
2024-05-28 11:45:46 +0545

    Routing path set for every components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "products", component: ProductListComponent
+  },
+  {
+    path: "products:id", component: ProductDetailComponent
+  },
+  {
+    path: "products:id/edit", component: ProductEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
