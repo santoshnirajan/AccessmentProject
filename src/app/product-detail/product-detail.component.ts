a7f175cf291e4e69f88d1411706262c6ce00c640
santoshnirajan <santosh.shahi475@gmail.com>
2024-05-28 19:21:25 +0545

    Product Detail created and functionality implemented for Product Detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../service/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
+  productId: number = 0;
+  product: Product = new Product();
+  constructor(
+    private _productService: ProductService,
+    private _route: ActivatedRoute
+  ) {
+    this._route.paramMap.subscribe(
+      params => {
+        const idParam = params.get('id');
+        if (idParam !== null) {
+          this.productId = +idParam;
+        }
+      }
+    );
+    this.getSelectedProductDetail();
+  }
+
+  getSelectedProductDetail() {
+    this._productService.getProductById(this.productId).subscribe(
+      {
+        next: (res: any) => {
+          this.product = res;
+        },
+        error: (err) => {
+          console.log("error", err);
+        },
+        complete: () => {
+          console.log("Product Detailing Completed");
+        }
+      });
+  }
 
 }
